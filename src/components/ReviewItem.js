4a124cd2849b59567abf65f2fb33df00be28448e
seungkyu2 <zx6658@naver.com>
2018-01-19 16:20:09 +0900

    ui update

--- src/components/ReviewItem.js
+++ src/components/ReviewItem.js
@@ -12,25 +12,32 @@ function ReviewItem(props) {
   let Time = moment(new Date(ratedTime)).format('YYYY년 MM월 DD일 HH:mm')
   console.log(Time);
   return (
+
     <div className="review-item">
-      <div className="review-title">
-        {menu.shortName}
-      </div>
       <div className="review-detail">
-        {' '}
-        <strong>{comment} </strong><br/>
-        {ratedTime}
+        {ratedTime}<br/>
       </div>
-      <div className="review-bottom">
-        {' '}
+      <div className="review-title">
         <StarRating
           className={cx(`MenuDetail-review-StarRating`)}
           rating={rating}
           color='#ef8e2b'
           size="1.2rem"
                         />
+        {menu.shortName}
+      </div>
+      <div className="review-bottom">
+        {' '}
+
         {' '}
       </div>
+
+      <div className="review-detail">
+        {' '}
+        <strong>{comment} </strong>
+
+      </div>
+
 
       <div>
 
